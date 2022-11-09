a514cfcc9987c95a1b5c7dd858ba6aa284f4c420
DrAbdulhadi <miamiv21@gmail.com>
2022-11-10 02:56:57 +0300

    First upload

---
+++ app.js
@@ -0,0 +1,185 @@
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const fs = require('fs');
+const path = require('path');
+const mongoose = require("mongoose");
+require('dotenv/config')
+
+const app = express();
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    })
+    );
+    app.use(bodyParser.json());
+app.set("view engine", "ejs");
+    
+app.use(express.static("public"));
+
+const databaseSchema = new mongoose.Schema({
+    user: {
+       
+        date: Date,
+        name: String ,
+         address: String,
+         phoneNumber: Number,
+         length: String,
+         kitchenType: String ,
+         upSide: Boolean ,
+    }
+        
+});
+
+const Alitimad=new mongoose.model("AlItimad",databaseSchema)
+//$       Image Upload          //
+
+let multer = require("multer");
+
+let storage = multer.diskStorage({
+    destination: (req, file, cb) =>
+    {
+        cb(null, "uploads")
+    },
+    filename: (req, file, cb) =>
+    {
+        cb(null, file.fieldname + "-" + Date.now())
+    }
+});
+
+let upload = multer({ storage: storage });
+
+var imgModel = require("./model");
+
+
+app.get("/Admin", (req, res) =>
+{
+    imgModel.find({}, (err, items) =>
+    {
+        if (err)
+        {
+            console.log(err);
+            res.status(500).send("An Error Occured", err);
+        } else
+        {
+            res.render("imagesPage", { items: items });
+        }
+    });
+});
+
+app.post("/", upload.single("image"), (req, res, next) =>
+{
+    var obj = {
+        name: req.body.name,
+        desc: req.body.desc,
+        img: {
+            data: fs.readFileSync(path.join(__dirname + "/uploads/" + req.file.filename)),
+            contentType: "image/png"
+        }
+    }
+    imgModel.create(obj, (err, item) =>
+    {
+        if (err)
+        {
+            console.log(err);
+        } else
+        {
+            item.save();
+            res.redirect("/")
+        }
+    })
+});
+//#                               //
+
+
+
+//$         Database Declarations   //
+mongoose.connect(process.env.MONGO_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+}).then(() =>
+{
+    console.log('connected-to-DB');
+}).catch(err =>
+{
+    console.log(err.message)
+});
+
+
+
+app.get("/", function (req, res)
+{
+    res.render("index")
+    
+});
+app.get("/admin/orders", function (req, res)
+{
+    
+    
+    
+    Alitimad.find({}, function (err, ordrs)
+    {
+        // console.log(ordrs);
+        
+        res.render("orders", {
+            orders: ordrs
+        });
+    });
+    
+});
+
+app.post("/callOrders", function (req, res)
+{
+    let date = new Date();
+    let name = req.body.name;
+    let phone = req.body.phone;
+    let address = req.body.address;
+    let length = req.body.length;
+    let top = req.body.top;
+    let type = req.body.type
+    
+    // console.log(req.body, type);
+    // Alitimad.find({},function (err,obj) {
+    //     console.log(obj);
+    // })
+    var registerOrder = new Alitimad({
+        user: {
+            
+            date: date,
+            name: name   ,
+             address: address,
+             phoneNumber: phone,
+             length: length,
+             kitchenType: type ,
+             upSide: true ,
+        }
+    })
+    registerOrder.save();
+    setTimeout(() => res.redirect("/"), 2660);
+});
+
+
+//$         GET & POST Requests     //
+// app.get("/main", function (req, res)
+// {
+//     res.render("index.html")
+// });
+
+// app.get("/Admin", function (req, res)
+// {
+    
+// });
+
+// app.post("/infoRequest", function (req, res)
+// {
+    
+// });
+
+
+
+
+//$          Listening Port         //
+app.listen(process.env.PORT || 3000, function ()
+{
+    console.log("Server Started on port 3000")
+});
---
+++ public/script.js
@@ -0,0 +1,96 @@
+"use strict";
+
+class Caro {
+  constructor(el) {
+    this.el = el;
+    this.caroData = [
+      {
+        id: "1",
+        src: "imgs/img1.jpg",
+      },
+      {
+        id: "2",
+        src: "imgs/img2.jpg",
+      },
+      {
+        id: "3",
+        src: "imgs/img3.jpg",
+      },
+      {
+        id: "4",
+        src: "imgs/img4.jpg",
+      },
+      {
+        id: "5",
+        src: "imgs/img5.jpg",
+      },
+    ];
+    this.caroInView = [1, 2, 3, 4, 5];
+    this.caroContainer;
+    this.caroPlayState;
+  }
+
+  mounted() {
+    this.setupCaro();
+  }
+
+  setupCaro() {
+    const conta = document.createElement("div");
+    const contro = document.createElement("div");
+
+    this.el.append(conta, contro);
+    conta.className = "cc-container";
+
+    this.caroData.forEach((item, index) => {
+      const caroItem = item.src
+        ? document.createElement("img")
+        : document.createElement("div");
+
+      conta.append(caroItem);
+
+      caroItem.className = `cc-item cc-item-${index + 1}`;
+      caroItem.src = item.src;
+      caroItem.setAttribute("loading", "lazy");
+      caroItem.setAttribute("data-index", `${index + 1}`);
+    });
+    this.caroConta = conta;
+  }
+
+  previouss() {
+    this.caroData.unshift(this.caroData.pop());
+
+    this.caroInView.push(this.caroInView.shift());
+
+    this.caroInView.forEach((item, index) => {
+      this.caroConta.children[index].className = `cc-item cc-item-${item}`;
+    });
+
+    this.caroData.slice(0, 5).forEach((data, index) => {
+      document.querySelector(`.cc-item-${index + 1}`).src = data.src;
+    });
+  }
+
+  nextt() {
+    this.caroData.push(this.caroData.shift());
+
+    this.caroInView.unshift(this.caroInView.pop());
+
+    this.caroInView.forEach((item, index) => {
+      this.caroConta.children[index].className = `cc-item cc-item-${item}`;
+    });
+
+    this.caroData.slice(0, 5).forEach((data, index) => {
+      document.querySelector(`.cc-item-${index + 1}`).src = data.src;
+    });
+  }
+}
+const el = document.querySelector(".cc");
+const exampleCarousel = new Caro(el);
+exampleCarousel.mounted();
+document.querySelector(".nextbtn").addEventListener("click", function (e) {
+  exampleCarousel.nextt();
+});
+document.querySelector(".previousbtn").addEventListener("click", function (e) {
+  exampleCarousel.previouss();
+});
+// ---------------------
